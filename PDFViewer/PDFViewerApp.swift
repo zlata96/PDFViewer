b9eeb1380493e8845f7934abe28e1c0e91e0ddd7
Zlata Guseva <gusevazlata96@gmail.com>
2023-10-15 13:57:23 +0400

    Initial Commit

---
+++ PDFViewer/PDFViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewerApp.swift
+//  PDFViewer
+//
+//  Created by Злата Гусева on 15.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
